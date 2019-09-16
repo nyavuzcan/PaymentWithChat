9b18286177e067cdb2f0a16d558ee9242d8cb166
Nevzat YAVUZCAN <nyavuzcan@windowslive.com>
2019-09-16 13:40:55 +0300

    second commit

--- app/src/main/java/com/example/myapplication/dogrulama.java
+++ app/src/main/java/com/example/myapplication/dogrulama.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class dogrulama extends Activity {
     Context context=this;
@@ -22,6 +23,7 @@ public class dogrulama extends Activity {
             public void onClick(View v) {
                 Intent intent= new Intent(getApplicationContext(),Bryselgiris.class);
                 startActivity(intent);
+                Toast.makeText(context,"Git",Toast.LENGTH_LONG).show();
             }
         });
     }
